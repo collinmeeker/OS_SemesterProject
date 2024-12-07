65bcb96c03cfea99910f0888990a68c18d199e46
Collin Meeker <collinmeeker1@gmail.com>
2024-12-06 21:28:28 -0500

    HW2 and HW1 Fixes

--- kernel.c
+++ kernel.c
@@ -5,16 +5,9 @@
 void main() {
 	
 	clear_terminal();
-	print_string("HELLO");
-	print_line("WORLD");
-	print_string("TODAY");	
-		
-	//char* const VGA_BUFFER = (char*) 0xb8000;
-	//char* str = "Hello World";
-
-	//for (int i=0; str[i] != '\0'; i++) {
-	//	VGA_BUFFER[i*2] = str[i];
-	//}
-	
+	print_string_with_color("hello", YELLOW,CYAN);
+	print_line_with_color("World", MAGENTA, GREEN);
+	print_string("Today");	
+			
 	return;
 }
